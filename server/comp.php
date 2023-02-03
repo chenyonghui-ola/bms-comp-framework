3044dd864e06549a60884f792db68dddae62c8ee
chenyonghui <chen.yonghui1@olaola.chat>
2023-02-03 10:22:56 +0800

    op

---
+++ server/comp.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 更新组件库
+ * Date: 2022-02-02
+ * Version: 1.0.0
+ */
+
+$config = "./comp.ini";
+$configData = parse_ini_file($config,true);
+
+//支持新增
+
+//支持删除
+
+//支持全量更新 判断当前版本号是否一致一样的不更新
+
+//支持按模块名称更新 判断当前版本号是否一致一样的不更新
+
+//如果模块有autoload_file配置需要更新到comp_autoload.php文件里
+
+//更新完成后生成一个version文件记录当前版本号，如果版本号为空的始终允许更新
+
+
